46b6ddeab3c0c793c756d7db7c549ab2074c8f1d
gongzhen <517441540@qq.com>
2024-10-09 22:06:49 +0800

    feat: add memory test

---
+++ src/memory.py
@@ -0,0 +1,32 @@
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
+import os
+
+os.environ['OPENAI_API_BASE'] = 'https://api.chatanywhere.com.cn/v1'
+
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+
+template_str = """
+You are a chatbot having a conversation with a human.
+Previous conversation:
+{chat_history}
+Human: {question}
+AI:"""
+
+prompt_template = PromptTemplate.from_template(template_str)
+
+llm = ChatOpenAI(temperature=0)
+
+memory_chain = LLMChain(
+    llm=llm,
+    prompt=prompt_template,
+    verbose=True, 
+    memory=memory,
+)
+
+
+print(memory_chain.predict(question="你好，我是jack"))
+print(memory_chain.predict(question="你还记得我叫什么吗？"))
---
+++ src/test01.py
@@ -0,0 +1,27 @@
+from langchain.output_parsers import CommaSeparatedListOutputParser
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+import os
+
+os.environ['OPENAI_API_BASE'] = 'https://api.chatanywhere.com.cn/v1'
+
+# 1. 实例化一个 CommaSeparatedListOutputParser对象
+output_parser = CommaSeparatedListOutputParser()
+
+format_instructions = output_parser.get_format_instructions()
+
+# 2. 创建一个 prompt template，将 output_parser.get_format_instructions()填充进最终的prompt中
+prompt = PromptTemplate(
+    template="List five {subject}.\n{format_instructions}",
+    input_variables=["subject"],
+    partial_variables={"format_instructions": format_instructions},
+)
+
+# 3. 创建一个LLM实例
+model = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+# 4. 构建链
+chain = prompt | model | output_parser
+
+print(chain.invoke({"subject": "彩虹颜色"}))
